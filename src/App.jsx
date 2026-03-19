894fc124cf2f8445af271a323f6143b4eed9134d
xKrisek <krzysiekb3327@gmail.com>
2026-03-19 14:02:24 +0100

    Co-authored-by: TymTom333 <TymTom333@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter, Routes, Link, Route } from 'react-router'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <BrowserRouter>
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/baza-wiedzy">About</Link>
+        <Link to="/narzedzie">Contact</Link>
+      </nav>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/baza-wiedzy" element={<bazaWiedzy />} />
+        <Route path="/narzedzie" element={<Narzedzie />} />
+      </Routes>
+    </main>
 
-      <div className="ticks"></div>
-    </>
+    </BrowserRouter>
   )
 }
 
