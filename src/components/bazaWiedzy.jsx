a113c92db45ce44e197f83c5de12d9c0749910b8
TymTom333 <tymoteusz.klimczak@onet.pl>
2026-03-20 14:05:58 +0100

    14 05

--- src/components/bazaWiedzy.jsx
+++ src/components/bazaWiedzy.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react'
 import './bazaWiedzy.css'
 
-function BazaWiedzy()
-{
+function BazaWiedzy() {
+    
+    const topics = ['WSTĘP', 'DLACZEGO TE JEST WAŻNE', 'CZYNNIKI STARZENIOWE', 'SŁOWNICZEK', 'KTO SKORZYSTA Z TEJ WIEDZY?', 'ZAWODY OFFSHORE I OUTDOOR', 'DOBÓR ŚOI']
+
+    const [selectedTopic, setSelectedTopic] = useState('WSTĘP')
+
+
     return(
         <>
             <h1 id='baza-wiedzy-title'>Baza wiedzy</h1>
             <div id='baza-wiedzy-buttons'>
-                <button id='baza-wiedzy-button'>WSTĘP</button><br/>
-                <button id='baza-wiedzy-button'>DLACZEGO TE JEST WAŻNE</button><br/>
-                <button id='baza-wiedzy-button'>CZYNNIKI STARZENIOWE</button><br/>
-                <button id='baza-wiedzy-button'>SŁOWNICZEK</button><br/>
-                <button id='baza-wiedzy-button'>KTO SKORZYSTA Z TEJ WIEDZY?</button><br/>
-                <button id='baza-wiedzy-button'>ZAWODY OFFSHORE I OUTDOOR</button><br/>
-                <button id='baza-wiedzy-button'>DOBÓR ŚOI</button>
+                {topics.map((topic) => {
+                    return(
+                        <button id={selectedTopic === topic ? 'baza-wiedzy-button_selected' : 'baza-wiedzy-button'} onClick={() => setSelectedTopic(topic)}>{topic}</button>
+                    )
+                })}
+            </div>
+            <div id='baza-wiedzy-tresc'>
+                {selectedTopic === 'WSTĘP' && <div>Witamy w naszej bazie wiedzy, stworzonej z myślą o wszystkich zainteresowanych tematyką bezpieczeństwa w środowisku off-shore i outdoorowym. Naszym celem jest dostarczenie rzetelnych informacji, które pomogą zrozumieć, jak ważne jest odpowiednie dobieranie i ocenianie stanu technicznego ŚOI (Środków Ochrony Indywidualnej) stosowanych w tych wymagających warunkach.</div>}
             </div>
         </>        
     )
